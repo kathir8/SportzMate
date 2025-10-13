bed2847d30c93c5e53f8ccc08978b95eedff7f37
kathiravan <kathiravans@boodskap.io>
2025-10-13 07:22:59 +0530

    fab button

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonIcon, IonTabs, IonTabBar, IonTabButton } from '@ionic/angular/standalone';
-import { chatbubblesSharp, homeSharp, mailOpenSharp, menuOutline } from 'ionicons/icons';
+import { IonIcon, IonTabs, IonTabBar, IonTabButton,IonFab,IonFabButton } from '@ionic/angular/standalone';
+import { chatbubblesSharp, homeSharp, mailOpenSharp, menuOutline, add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
-  imports: [IonIcon, IonTabs, IonTabBar, IonTabButton, FormsModule]
+  imports: [IonIcon, IonTabs, IonTabBar, IonTabButton, FormsModule,IonFab,IonFabButton]
 })
 export class DashboardComponent {
-  icons = { homeSharp, chatbubblesSharp, menuOutline, mailOpenSharp };
+  icons = { homeSharp, chatbubblesSharp, menuOutline, mailOpenSharp, add };
 
 }
--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonTitle, IonIcon, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonTitle, IonIcon, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel,IonFab,IonFabButton,IonFabList } from '@ionic/angular/standalone';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { navigateCircleOutline } from 'ionicons/icons';
+import { navigateCircleOutline, filterSharp } from 'ionicons/icons';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [IonContent, IonTitle, IonIcon, NgSelectModule, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel, FormsModule, CommonModule]
+  imports: [IonContent, IonTitle, IonIcon, NgSelectModule, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel, FormsModule, CommonModule,IonFab,IonFabButton,IonFabList]
 
 })
 export class HomeComponent {
-  icons = { navigateCircleOutline };
+  icons = { navigateCircleOutline, filterSharp };
   countries = [{ code: 'ad', name: 'Andorra' },
   { code: 'ae', name: 'United Arab Emirates' },
   { code: 'af', name: 'Afghanistan' },
