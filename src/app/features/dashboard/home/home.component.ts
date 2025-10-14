05d7a483373720be6f69abb22ff17b5da10edc8b
kathiravan <kathiravans@boodskap.io>
2025-10-14 07:14:15 +0530

    sportzamate list

--- src/app/features/dashboard/home/home.component.ts
+++ src/app/features/dashboard/home/home.component.ts
@@ -1,24 +1,69 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonTitle, IonIcon, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel,IonFab,IonFabButton,IonFabList, IonRange } from '@ionic/angular/standalone';
+import { IonContent, IonTitle, IonIcon, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel, IonFab, IonFabButton, IonFabList, IonRange, IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, InfiniteScrollCustomEvent, IonThumbnail, IonGrid, IonRow, IonCol, IonBadge } from '@ionic/angular/standalone';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { navigateCircleOutline, filterSharp } from 'ionicons/icons';
+import { navigateCircleOutline, filterSharp, calendarClear, peopleOutline, bicycleOutline } from 'ionicons/icons';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [IonContent, IonTitle, IonIcon, NgSelectModule, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel, FormsModule, CommonModule,IonFab,IonFabButton,IonFabList,IonRange]
+  imports: [IonContent, IonTitle, IonIcon, NgSelectModule, IonImg, IonAvatar, IonSegment, IonSegmentButton, IonSegmentView, IonSegmentContent, IonLabel, FormsModule, CommonModule, IonFab, IonFabButton, IonFabList, IonRange, IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonThumbnail, IonGrid, IonRow, IonCol, IonBadge]
 
 })
 export class HomeComponent {
-  icons = { navigateCircleOutline, filterSharp };
+  icons = { navigateCircleOutline, filterSharp, calendarClear, peopleOutline, bicycleOutline };
   countries = [{ code: 'ad', name: 'Andorra' },
   { code: 'ae', name: 'United Arab Emirates' },
   { code: 'af', name: 'Afghanistan' },
   { code: 'ag', name: 'Antigua and Barbuda' }];
 
   segmentView: 'list' | 'map' = "list";
+  sportsMateList = [{
+    icon: '',
+    name: 'Christian Bale',
+    place: 'Chicago',
+    time: '20 Oct, 4 PM',
+    need: 8,
+    coming: 8,
+    distanceOrDuration: '600 M',
+    type: 'Cycling'
+  },
+  {
+    icon: '',
+    name: 'Elizabeth Taylor',
+    place: 'Naperville',
+    time: '20 Oct, 4 PM',
+    need: 3,
+    coming: 4,
+    distanceOrDuration: '2 Hr',
+    type: 'Badmiton'
+  },
+  {
+    icon: '',
+    name: 'Judy',
+    place: 'Bolingbrook',
+    time: '20 Oct, 4 PM',
+    need: 4,
+    coming: 4,
+    distanceOrDuration: '3 km',
+    type: 'Running'
+  }]
+
+  formatKm(value: number) {
+    return `${value} KM`;
+  }
+
+  private generateItems() {
+
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.generateItems();
+    setTimeout(() => {
+      event.target.complete();
+    }, 500);
+  }
 }
