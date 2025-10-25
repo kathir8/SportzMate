487c73ab7001219138d8ee5a3651273f2d1cf68d
kathiravan <kathiravans@boodskap.io>
2025-10-26 00:08:34 +0530

    infinte scroll

--- src/app/features/dashboard/home/mate-stuff/mate-list-view/mate-list-view.component.ts
+++ src/app/features/dashboard/home/mate-stuff/mate-list-view/mate-list-view.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, effect, inject, input, signal } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonThumbnail, IonGrid, IonRow, IonCol, IonBadge, IonLabel, IonIcon, InfiniteScrollCustomEvent } from '@ionic/angular/standalone';
+import { IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonThumbnail, IonGrid, IonRow, IonCol, IonBadge, IonLabel, IonIcon, InfiniteScrollCustomEvent, IonContent } from '@ionic/angular/standalone';
 import { add, bicycleOutline, calendarClear, peopleOutline } from 'ionicons/icons';
 import { MateListItem } from '../models/mate.model';
 import { NoMateFoundComponent } from "../no-mate-found/no-mate-found.component";
@@ -10,7 +10,7 @@ import { DATE_FORMATS } from 'src/app/core/constants';
   selector: 'app-mate-list-view',
   templateUrl: './mate-list-view.component.html',
   styleUrls: ['./mate-list-view.component.scss'],
-  imports: [IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonThumbnail, IonGrid, IonRow, IonCol, IonBadge, IonLabel, IonIcon, NoMateFoundComponent, LocalTimePipe]
+  imports: [IonList, IonItem, IonInfiniteScroll, IonInfiniteScrollContent, IonThumbnail, IonGrid, IonRow, IonCol, IonBadge, IonLabel, IonIcon, NoMateFoundComponent, LocalTimePipe, IonContent]
 })
 export class MateListViewComponent {
   private router = inject(Router);
@@ -18,7 +18,21 @@ export class MateListViewComponent {
   icons = { peopleOutline, bicycleOutline, calendarClear, add };
 
   visiblePlayers = input<MateListItem[]>([]);
+  displayedPlayers = signal<MateListItem[]>([]);
 
+
+  private batchSize = 9;
+  private loadedCount = 0;
+
+
+  constructor() {
+    effect(() => {
+      const allPlayers = this.visiblePlayers();
+      this.loadedCount = 0;
+      this.displayedPlayers.set([]);
+      this.loadMorePlayersBatch(allPlayers); // load first batch
+    });
+  }
 
   openMateDetail(id: number) {
     // Pass the ID or name in the route parameter
@@ -26,9 +40,26 @@ export class MateListViewComponent {
   }
 
   loadMore(event: InfiniteScrollCustomEvent) {
-    setTimeout(() => {
+    this.loadMorePlayersBatch(this.visiblePlayers(), event);
+  }
+
+  loadMorePlayersBatch(allPlayers: MateListItem[], event?: InfiniteScrollCustomEvent) {
+
+    const nextBatch = allPlayers.slice(this.loadedCount, this.loadedCount + this.batchSize);
+    if (nextBatch.length) {
+      this.displayedPlayers.update(arr => {
+        arr.push(...nextBatch);
+        return arr;
+      });
+      this.loadedCount += nextBatch.length;
+    }
+
+    if (event) {
+      if (this.loadedCount >= allPlayers.length) {
+        (event.target as HTMLIonInfiniteScrollElement).disabled = true;
+      }
       event.target.complete();
-    }, 500);
+    }
   }
 
 }
