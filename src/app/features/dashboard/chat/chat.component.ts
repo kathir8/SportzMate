2a43ca3968dbb178273074611d01cc7b6fdc5ff7
Kathiravan <sukn99@gmail.com>
2025-12-08 06:12:35 +0530

    emoji enabled

--- src/app/features/dashboard/chat/chat.component.ts
+++ src/app/features/dashboard/chat/chat.component.ts
@@ -1,25 +1,51 @@
-import { Component, computed, effect, inject, Signal, signal } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Component, computed, inject, Signal, signal } from '@angular/core';
+import { IonContent, IonIcon } from '@ionic/angular/standalone';
 import { ChatMessage, ChatService } from './chat.service';
 import { UserStore } from 'src/app/core/stores/user-store';
 import { FormsModule } from '@angular/forms';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { switchMap } from 'rxjs';
+import { happyOutline, attachOutline, sendOutline, closeOutline } from 'ionicons/icons';
+import { IonicInputComponent } from "src/app/shared/components/ionic-input/ionic-input.component";
 import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
-  imports: [IonContent, FormsModule,DatePipe]
+  imports: [IonContent, FormsModule, DatePipe, IonIcon, IonicInputComponent]
 })
 export class ChatComponent {
 
   private chatService = inject(ChatService);
   private userStore = inject(UserStore);
+  icons = { happyOutline, attachOutline, sendOutline, closeOutline };
 
   currentUid = signal(this.userStore.getCurrent()?.id ?? '');
   receiverUid = signal('');
+  showEmojiPicker = signal(false);
+  emojiList = signal([
+    "😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇",
+    "🙂", "🙃", "😉", "😌", "😍", "🥰", "😘", "😗", "😙", "😚",
+    "😋", "😛", "😝", "😜", "🤪", "🤨", "🧐", "🤓", "😎", "🤩",
+    "🥳", "😏", "😒", "😞", "😔", "😟", "😕", "🙁", "☹️", "😣",
+    "😖", "😫", "😩", "🥺", "😢", "😭", "😤", "😠", "😡", "🤬",
+    "🤯", "😳", "🥵", "🥶", "😱", "😨", "😰", "😥", "😓", "🤗",
+    "🤔", "🤭", "🤫", "🤥", "😶", "😐", "😑", "😬", "🙄", "😯",
+    "😦", "😧", "😮", "😲", "🥱", "😴", "🤤", "😪", "😵", "🤐",
+    "🥴", "🤢", "🤮", "🤧", "😷", "🤒", "🤕", "🤑", "🤠", "😈",
+    "👿", "👹", "👺", "🤡", "💩", "👻", "💀", "☠️", "👽", "👾",
+    "🤖", "🎃", "😺", "😸", "😹", "😻", "😼", "😽", "🙀", "😿",
+    "😾", "👋", "🤚", "🖐", "✋", "🖖", "👌", "🤏", "✌️", "🤞",
+    "🤟", "🤘", "🤙", "👈", "👉", "👆", "🖕", "👇", "☝️", "👍",
+    "👎", "✊", "👊", "🤛", "🤜", "👏", "🙌", "👐", "🤲", "🤝",
+    "🙏", "✍️", "💅", "🤳", "💪", "🦾", "🦿", "🦵", "🦶", "👂",
+    "🦻", "👃", "🧠", "🦷", "🦴", "👀", "👁", "👅", "👄", "💋",
+    "🩸", "❤️", "🧡", "💛", "💚", "💙", "💜", "🤎", "🖤", "🤍",
+    "💔", "❣️", "💕", "💞", "💓", "💗", "💖", "💘", "💝", "💟",
+    "🔥", "✨", "🌟", "💫", "💥", "💢", "💦", "💧", "💤", "🕳",
+    "🎉", "🎊", "🎈", "🎂", "🎁", "🕯", "💣"
+  ]);
   roomId = computed(() =>
     this.chatService.getRoomId(this.currentUid(), this.receiverUid())
   );
@@ -36,13 +62,17 @@ export class ChatComponent {
 
   constructor() {
 
-     const navState = history.state as { receiverUid?: string };
+    const navState = history.state as { receiverUid?: string };
 
     if (navState.receiverUid) {
       this.receiverUid.set(navState.receiverUid);
     }
   }
 
+
+  handleEmojiClick(emoji: string) {
+    this.newMessage.set(this.newMessage() + emoji);
+  }
 
   send() {
     if (!this.newMessage().trim()) return;
@@ -56,8 +86,8 @@ export class ChatComponent {
     this.newMessage.set('');
   }
 
-  getMsgSide(senderId:string):'sender' | 'reciever'{
-   return senderId === this.currentUid() ? 'sender' : 'reciever';
+  getMsgSide(senderId: string): 'sender' | 'reciever' {
+    return senderId === this.currentUid() ? 'sender' : 'reciever';
   }
 
   isMessageFirstInGroup(currentSenderIsMe: string, index: number): boolean {
