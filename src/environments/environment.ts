5dd7fe73a83e1b43eaf9396dd0c1e2c0bf33546a
kathiravan <kathiravans@boodskap.io>
2026-03-01 07:36:10 +0530

    environment update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const  environment = {
   production: false,
-  googleWebClientId: "853532937748-7fb6egmr2jhqt726pb641t995io06rda.apps.googleusercontent.com",
+  googleWebClientId: "1077081120161-4p0g76mn13ghks8dqnjai39n2qrgm0sp.apps.googleusercontent.com",
   firebaseConfig: {
     apiKey: "AIzaSyAndy7XYPM3jk_zy_Fyp6l4oeJHTjXhfVM",
     authDomain: "sportzmate-89ad8.firebaseapp.com",
